2c04f8761ed9431f8e36c0319cfdc9348e6e47f2
Ayrton Gomes <com.ayrton@gmail.com>
2018-04-09 22:46:37 -0100

    working on stuff

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import GoogleApiWrapper from './components/map';
 import './App.css';
 
+var web3 = require('web3');
+
 class App extends Component {
     render() {
         return (
             <div className="App">
-                {/* <GoogleApiWrapper /> */}
+                <GoogleApiWrapper />
             </div>
         );
     }
