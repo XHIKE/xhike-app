755a995634c3d893722b6bbfde8c14a77f161f53
Ayrton Gomes <com.ayrton@gmail.com>
2018-04-09 21:20:50 -0100

    show footer on map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import GoogleApiWrapper from './components/map';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <GoogleApiWrapper />
+                {/* <GoogleApiWrapper /> */}
             </div>
         );
     }
