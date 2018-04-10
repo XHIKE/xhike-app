730859db741a2b280980912f2701b2306d14c4a2
Ayrton Gomes <com.ayrton@gmail.com>
2018-04-10 02:03:57 -0100

    add info icon to counter component

--- src/components/counter.js
+++ src/components/counter.js
@@ -17,9 +17,14 @@ export default class Counter extends Component {
             <div className="col-sm-4">
                 <div className="info-box">
                     <span className={this.props.color + ' info-box-icon'}><i className={this.props.icon}></i></span>
-                    <div className="info-box-content">
-                        <span className="info-box-text">{this.props.label}</span>
-                        <span className="info-box-number">{this.props.count}</span>
+                    <div className="info-box-content">                        
+                        <span className="fa fa-info-circle pull-right" style={{color: '#f39c12', paddingTop: '4px'}}></span>
+                        <span className="info-box-text">
+                            {this.props.label}
+                        </span>
+                        <span className="info-box-number">
+                            {this.props.count}
+                        </span>
                     </div>
                 </div>
             </div>
